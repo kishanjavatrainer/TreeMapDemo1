c23a5b436e20ea4f2f3724ae87aef3990298039a
Thoeun Sopheara <45703961+thoeunsopheara@users.noreply.github.com>
2019-07-14 18:01:39 +0700

    Update ClientTest.java

--- TreeMapDemo1/src/com/infotech/client/ClientTest.java
+++ TreeMapDemo1/src/com/infotech/client/ClientTest.java
@@ -12,29 +12,34 @@ import com.infotech.model.Employee;
 public class ClientTest {
 
 	public static void main(String[] args) {
+		// create Employee class
+		System.out.println("Please enter employee 1 information:");
+		Employee e1 = new Employee();
+		System.out.println("Please enter employee 2 information:");
+		Employee e2 = new Employee();
+		System.out.println("Please enter employee 3 information:");
+		Employee e3 = new Employee();
+		System.out.println("Please enter employee 4 information:");
+		Employee e4 = new Employee();
+		System.out.println("Please enter employee 5 information:");
+		Employee e5 = new Employee();
 		
-		Employee e1 = new Employee(1009, "Sam", 90000.00, "sam@infotech.com");
-		Employee e2 = new Employee(2009, "Ambay", 30000.00, "martin@infotech.com");
-		Employee e3 = new Employee(3009, "Joya", 50000.00, "joya@infotech.com");
-		Employee e4 = new Employee(4009, "Boya", 70000.00, "harry@infotech.com");
-		Employee e5 = new Employee(5009, "Sean", 80000.00, "sean@infotech.com");
-		
-		
+		// create department class
 		Department d1 = new Department(10001, "IT", "Boston");
 		Department d2 = new Department(10002, "Finance", "Chennai");
 		
+		// create TreeMap
 		Map<Employee,Department> empDeptMap = new TreeMap<>();
 		
+		// assign key and value to TreeMap
 		empDeptMap.put(e1, d1);
 		empDeptMap.put(e2, d1);
 		empDeptMap.put(e3, d1);
-		
 		empDeptMap.put(e4, d2);
 		empDeptMap.put(e5, d2);
 		
-		//empDeptMap.put(null, d1);
 		
-		Set<Entry<Employee, Department>> entrySet = empDeptMap.entrySet();
+		Set<Entry<Employee, Department>> entrySet = empDeptMap.entrySet(); // to get set view
 		Iterator<Entry<Employee, Department>> iterator = entrySet.iterator();
 		while (iterator.hasNext()) {
 			Map.Entry<Employee, Department> entry = (Map.Entry<Employee,Department>) iterator.next();
@@ -42,6 +47,7 @@ public class ClientTest {
 			Department department = entry.getValue();
 			System.out.println(employee);
 			System.out.println(department);
+			System.out.println("===== ===== ===== ===== =====");
 		}
 	}
 }
